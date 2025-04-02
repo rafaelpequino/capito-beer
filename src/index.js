7024c18096486ae578614416344b6e13c5c1f002
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-04-02 13:56:02 -0300

    Remove espaço desnecessário

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import App from './App';
 import Entregas from './routes/Entregas';
 
