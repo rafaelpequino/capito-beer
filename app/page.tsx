ec2b9300062286b5135d93bacf9d78de80280839
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-05-25 15:01:38 -0300

    Cria component Welcome

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/Header";
+import Welcome from "@/components/Home/Welcome";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <main>
+        <Welcome />
+      </main>
     </div>
   );
 }
---
+++ components/Home/Welcome/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Welcome() {
+    return (
+        <section
+            className="px-4 relative mt-18 w-full h-[500px] flex items-center justify-center bg-[url('/beer.jpg')] bg-cover bg-center overflow-hidden"
+        >
+            <div className="absolute inset-0 bg-black/60 z-0" />
+
+            <div className="w-full z-10 max-w-[1280px] text-white">
+                <div className="w-full max-w-[500px]">
+                    <h1 className="font-black">UM BRINDE AO EXTRAORDINÁRIO</h1>
+                    <h3 className="mt-2 mb-4">Cerveja com alma, sabor e história</h3>
+                    <Button className="bg-[var(--red)] hover:bg-[var(--red)] cursor-pointer">Venha brindar!</Button>
+                </div>
+            </div>
+        </section>
+    );
+}
